a086ed3b74dee99441e556259b9564afaaefe770
mohseni929 <mohseni929@gmail.com>
2022-04-10 22:29:36 -0700

    update create_table

--- Processing/create_tables.py
+++ Processing/create_tables.py
@@ -1,18 +1,19 @@
 import sqlite3 
- 
-conn = sqlite3.connect('stats.sqlite') 
- 
-c = conn.cursor() 
-c.execute(''' 
-          CREATE TABLE stats 
-          (id INTEGER PRIMARY KEY ASC,  
-           num_of_referees INTEGER NOT NULL, 
-           num_of_experience INTEGER NOT NULL, 
-           num_of_fans INTEGER, 
-           num_of_fields INTEGER, 
-           num_of_class INTEGER, 
-           last_updated VARCHAR(100) NOT NULL) 
-          ''') 
- 
-conn.commit() 
-conn.close()
+
+def create_database():
+    conn = sqlite3.connect('/data/data.sqlite')
+    c = conn.cursor() 
+    c.execute(''' 
+            CREATE TABLE stats 
+            (id INTEGER PRIMARY KEY ASC,  
+            num_of_referees INTEGER NOT NULL, 
+            num_of_experience INTEGER NOT NULL, 
+            num_of_fans INTEGER, 
+            num_of_fields INTEGER, 
+            num_of_class INTEGER, 
+            last_updated VARCHAR(100) NOT NULL) 
+            ''') 
+    
+    conn.commit() 
+    conn.close()
+    print("table created")
