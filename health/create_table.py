68500621377e99accba8c5e0e6986f8ef5795f72
mohseni929 <mohseni929@gmail.com>
2022-04-11 18:31:37 -0700

    dit create_tables on processing

--- health/create_table.py
+++ health/create_table.py
@@ -4,13 +4,13 @@ def create_database():
     conn = sqlite3.connect('/data/health.sqlite')
     c = conn.cursor()
     c.execute('''
-        CREATE TABLE health
-        (id INTEGER PRIMARY KEY ASC,
-        reciever VARCHAR(100) NOT NULL,
-        storage VARCHAR(100) NOT NULL,
-        processing VARCHAR(100) NOT NULL,
-        audit_log VARCHAR(100) NOT NULL,
-        last_updated VARCHAR(100) NOT NULL)
+            CREATE TABLE health
+            (id INTEGER PRIMARY KEY ASC,
+            reciever VARCHAR(100) NOT NULL,
+            storage VARCHAR(100) NOT NULL,
+            processing VARCHAR(100) NOT NULL,
+            audit_log VARCHAR(100) NOT NULL,
+            last_updated VARCHAR(100) NOT NULL)
     ''')
     conn.commit()
     conn.close()
